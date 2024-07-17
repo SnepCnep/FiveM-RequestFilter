a2e3d3d2138572a82ba94ab00703e4947ddc3eeb
SnepCnep <tijmenk2006@gmail.com>
2024-07-17 16:07:32 +0200

    add author oepssie

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Your Name'
-description 'Your Resource Description'
+author 'SnepCnep'
+description 'FiveM Http Filter'
 version '1.0.0'
 
 server_scripts {
